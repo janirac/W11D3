4494cb7f457fec6cb99321ad4463455f1031043b
Janira Crispin <janiracrispin@gmail.com>
2023-01-25 20:00:58 -0500

    testing

--- src/components/SelectedCoffeeBean.js
+++ src/components/SelectedCoffeeBean.js
@@ -1,5 +1,15 @@
-const SelectedCoffeeBean = () => {
-  return null;
+import { useContext} from "react";
+import { CoffeeContext } from "../context/CoffeeContext";
+
+const SelectedCoffeeBean = (props) => {
+  const {coffeeBean} = useContext(CoffeeContext)
+  console.log(coffeeBean)
+
+  return (
+    <div className="selected-coffee">
+    
+    </div>
+  )
 }
 
 export default SelectedCoffeeBean;
